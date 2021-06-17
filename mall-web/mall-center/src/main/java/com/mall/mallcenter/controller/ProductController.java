d15ede74eb9118af26c4702821cd3830b94f1b73
heping <28103524@qq.com>
2021-06-17 18:17:37 +0800

    新增商品页

--- mall-web/mall-center/src/main/java/com/mall/mallcenter/controller/ProductController.java
+++ mall-web/mall-center/src/main/java/com/mall/mallcenter/controller/ProductController.java
@@ -4,8 +4,10 @@ package com.mall.mallcenter.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.peace.api.ProductService;
 import com.peace.entity.Product;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,16 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author peace
  * @since 2021-06-15
  */
-@RestController
+@Controller
 @RequestMapping("product")
 public class ProductController {
     @Reference
     private ProductService productService;
 
     @RequestMapping("getProduct/{id}")
+    @ResponseBody
     public Product getProductById(@PathVariable("id") Long id){
 //        productService.getById(id)
         return productService.getProductById(id);
+    }
+
+    @RequestMapping("list")
+    public String list(){
+        return "product/list";
     }
 
 
